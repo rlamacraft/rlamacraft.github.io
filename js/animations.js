32f2ba9a069fa9c7a722f685c266d4fb8d2a6192
Robert <rlamacraft@gmail.com>
2015-11-15 18:12:45 +0000

    Loads, see desc.
    - Project Hugo Part Two page
    - Images
    - LESS tag (bye-bye jQuery)
    - Font size fixing, remove <br> use
    - Fix height of main header to full page
    - Alter vertical project panel on small devices from 100% to 95%
    - Added functions for filter and transform
    - Plus some more smaller stuff
    
    And, yes I know this should atleast be split up or if not in some
    branches but screw it. This is just my thing anyway.

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,6 @@
 $(document).ready( function() {
 
+  //project is opened
   $('.project').click(function() {
     var url = $(this).data("src");
     var projectViewer = $('#project-viewer');
@@ -9,15 +10,18 @@ $(document).ready( function() {
     projectViewer.load(url + " #project-wrapper", function() {
       //Once new content has loaded
       Prism.highlightAll(); //re-render code blocks
+      $('.image-fullscreen-open').click(function() { openImageFullscreen(this) });
     });
 
-    openDrawer($('#project-viewer-wrapper'), projectViewer, $("#project-viewer-close"), 1000);
+    openDrawer($('#project-viewer-wrapper'), projectViewer, $('#project-viewer-close'), 1000, 100, "-100%");
   });
 
+  //project is closed
   $('#project-viewer-close').click(function() {
-    closeDrawer($('#project-viewer-wrapper'), $('#project-viewer'), $("#project-viewer-close"),500);
+    closeDrawer($('#project-viewer-wrapper'), $('#project-viewer'), $('#project-viewer-close'), 500);
   });
 
+  $('#image-fullscreen-close').click(closeImageFullscreen);
 } );
 
 /*
@@ -26,17 +30,31 @@ $(document).ready( function() {
   for the compoennts in the drawer and faClose is a font awesome close icon.
   iconDelay is a period of time after the animation starts that the icon disappears.
 */
-function openDrawer(absParent, relContainer, faClose, iconDelay) {
-  absParent.css("z-index", "100");
+function openDrawer(absParent, relContainer, faClose, iconDelay, zIndex, translation) {
+  absParent.css("z-index", zIndex);
 
   relContainer.show(function() {
-    relContainer.css("transform", "translateX(-100%)").css("opacity", "1.0");
+    relContainer.css("transform", "translateX(" + translation + ")");
+    relContainer.css("opacity", "1.0");
     relContainer.attr('data-open', 'true');
   });
 
   setTimeout(function() {
       faClose.css("display", "block").css("opacity", "1.0");
   }, iconDelay);
+}
+
+function openImageFullscreen(openButton) {
+  $('#image-fullscreen').show(function() {
+    $('#image-fullscreen').addClass('show');
+    $('#image-fullscreen-close').addClass('show').show();
+    $('#image-fullscreen-image').attr("src", ($(openButton).data("imagesrc")));
+  });
+}
+
+function closeImageFullscreen() {
+  $('#image-fullscreen').removeClass('show').hide('slow');
+  $('#image-fullscreen-close').removeClass('show').hide();
 }
 
 /*
