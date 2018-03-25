b6cdfa485d9053cbe4f7310ba11dfa776756c1fd
Robert <rlamacraft@gmail.com>
2018-03-25 21:33:59 +0100

    It's another year, so naturally it's time for another redesign. More minimalism and more blue.

--- js/dark-theme.js
+++
@@ -1,8 +0,0 @@
-/***
-** If the local time is after 19:00 or before 7:00 then display the dark-theme by appending the class 'dark-theme' to the root HTML tag
-***/
-var htmlRoot = document.getElementsByTagName("html")[0];
-var currentHour = new Date().getHours();
-if(currentHour >= 19 || currentHour <= 6) {
-  htmlRoot.classList.add("dark-theme");
-}
